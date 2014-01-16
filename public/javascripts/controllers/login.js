9993511fb62161bb7ad9ddeecef039f86ec8be19
CamShaft <bytheway.cameron@gmail.com>
2014-01-16 04:50:52 -0700

    update simple-ui

--- public/javascripts/controllers/login.js
+++ public/javascripts/controllers/login.js
@@ -1,17 +1,21 @@
+/**
+ * Module dependencies
+ */
 
-var app = require('..');
 var cookie = require('cookie');
 
-function LoginController($scope) {
-  $scope.setSecret = function(err, res) {
-    if (err) return;
-    cookie('_access_token', $scope.values.secret);
-    // TODO somehow refresh all of the current apis...
-    window.location.reload();
-  };
-}
+module.exports = function(app) {
+  function LoginController($scope) {
+    $scope.setSecret = function(err, res) {
+      if (err) return;
+      cookie('_access_token', $scope.values.secret);
+      // TODO somehow refresh all of the current apis...
+      window.location.reload();
+    };
+  }
 
-app.controller('LoginController', [
-  '$scope',
-  LoginController
-]);
+  app.controller('LoginController', [
+    '$scope',
+    LoginController
+  ]);
+};
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,4 +2,21 @@
  * Module dependencies
  */
 
-module.exports = require('simple-ui')('hyper-chess', []);
+var app = module.exports = require('simple-ui')('hyper-chess', [
+  require('ng-hyper-emitter-ws').name
+], require);
+
+/**
+ * Initialize the controller
+ */
+
+app.initController('login');
+
+/**
+ * Start the app
+ */
+
+app.start(function($injector) {
+  var emitter = $injector.get('hyperEmitterWs');
+  emitter({port: 80, host: 'hyper-emitter.herokuapp.com'});
+});
